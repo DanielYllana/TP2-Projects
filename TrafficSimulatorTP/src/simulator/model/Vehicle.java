4aeae95d8416a15fc0e6e17c04b859b21be44b6a
corch <martin.crvm@gmail.com>
2022-02-08 09:48:21 +0100

    test 2

---
+++ TrafficSimulatorTP/src/simulator/model/Vehicle.java
@@ -0,0 +1,58 @@
+package simulator.model;
+
+public class Vehicle extends SimulatedObject{
+        private List<Junction> list;  // to complete well //don´t have set or get yet
+        private int maximumSpeed;
+        private int currentSpeed;
+        private VehicleStatus  status;
+        private Road road; //should be null if is not in any road //to declare road
+        private int location;
+        private int contaminationClass; // number between 0 and 1 -> Specification not defined
+    //maxSpeed
+        void setMaximumSpeed(int n){
+            this.maximumSpeed = n;
+        }
+        int getMaximumSpeed(){
+            return this.maximumSpeed;
+        }
+    //current speed
+        void setCurrentSpeed(int n){
+            this.currentSpeed = n;
+        }
+        int getCurrentSpeed(){
+            return this.currentSpeed;
+        }
+    //status
+        void setStatus(VehicleStatus  status){
+            this.status = status;
+        }
+        VehicleStatus getStatus(){
+            return this.status;
+        }
+    //road
+        void setRoad(Road  road){
+            this.road = road;
+        }
+        Road getRoad(){
+            return this.road;
+        }
+    //location
+        void setLocation(int  n){
+            this.location = n;
+        }
+        int getLocation(){
+            return this.location;
+        }
+     //contaminationClass
+        void setContaminationClass(int n){
+            this.contaminationClass = n;
+        }
+        int getContaminationClass(){
+            return this.contaminationClass;
+        }
+
+
+
+
+
+}
